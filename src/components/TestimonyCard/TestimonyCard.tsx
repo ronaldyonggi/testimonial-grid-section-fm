251e90606e946bef681fa7d5af1e964ad365700f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-07 14:10:56 +0800

    complete Testimony Card implementation

--- src/components/TestimonyCard/TestimonyCard.tsx
+++ src/components/TestimonyCard/TestimonyCard.tsx
@@ -6,12 +6,33 @@ interface TestimonyCardProps {
 }
 
 export default function TestimonyCard({ testimony }: TestimonyCardProps) {
+  const coloring = {
+    backgroundColor: `var(--color-${testimony.color})`,
+    color:
+      testimony.theme === 'dark'
+        ? 'var(--color-light-grayish-blue)'
+        : 'var(--color-very-dark-grayish-blue)',
+  };
+
+  const imageBorder = {
+    border: `4px solid var(--color-${testimony.color}-border)`,
+  };
+
   return (
-    <div>
-      <img src={testimony.image} alt='Profile picture of testimony author' />
-      <h2>{testimony.author}</h2>
-      <p>{testimony.reviewTitle}</p>
-      <blockquote>{testimony.reviewBody}</blockquote>
+    <div className={styles.container} style={coloring}>
+      <div className={styles.header}>
+        <img
+          src={testimony.image}
+          alt='Profile picture of testimony author'
+          style={imageBorder}
+        />
+        <div className={styles.nameWrapper}>
+          <h2 className={styles.name}>{testimony.author}</h2>
+          <p className={styles.verified}>Verified Graduate</p>
+        </div>
+      </div>
+      <p className={styles.quote}>{testimony.quote}</p>
+      <p className={styles.review}>{testimony.review}</p>
     </div>
   );
 }
