3bf6cb2d1767549f084c2c6aa7b659f03edfd5ea
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-07 14:11:14 +0800

    add container styling

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,9 +1,10 @@
 import { testimonies } from '../../testimonies';
 import TestimonyCard from '../TestimonyCard/TestimonyCard';
+import styles from './Section.module.css';
 
 export default function Section() {
   return (
-    <div>
+    <div className={styles.container}>
       {testimonies.map((testimony) => (
         <TestimonyCard key={testimony.author} testimony={testimony} />
       ))}
