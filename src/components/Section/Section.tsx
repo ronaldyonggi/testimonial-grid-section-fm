4f80d081ecd562451528cbe6aa6c210caac45fad
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-07 14:35:53 +0800

    mapping card now uses indexing

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -5,8 +5,12 @@ import styles from './Section.module.css';
 export default function Section() {
   return (
     <div className={styles.container}>
-      {testimonies.map((testimony) => (
-        <TestimonyCard key={testimony.author} testimony={testimony} />
+      {testimonies.map((testimony, index) => (
+        <TestimonyCard
+          key={index}
+          testimony={testimony}
+          className={styles[`card${index + 1}`]}
+        />
       ))}
     </div>
   );
